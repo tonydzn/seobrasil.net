d695e1a4e48103b8df9dc3a9fd600b79b22ec8ce
Tony Ananias <tonyananias@me.com>
2023-01-02 07:45:50 -0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://seobrasil.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
